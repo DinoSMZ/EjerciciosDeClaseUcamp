0dfa399f4d220bdb6a3804010d6079901505764d
DinoSMZ <dinoaldair@hotmail.com>
2022-06-09 00:49:30 -0500

    Finalizados

---
+++ Reto1 4 jun/main.js
@@ -0,0 +1,24 @@
+
+// reto 1 //
+//for (var i = 2; i<= 10; i++){
+//    if (i%2!==0){
+//    console.log(i)}
+//}
+
+
+// reto 2
+//for ( var i = 1; i<= 10; i++){
+//console.log(i*5)
+//}
+
+// reto 3
+
+//let Num = [1,2,45,5,6,3,1,8]
+//let Num = [1,1,2,3,5,6,8,45]
+//let Num = [100,5,5,8,3,7,8]
+//let Num = [3,5,5,7,8,8,100]
+
+//Num.sort(function (num1, num2) {
+
+  //  return num1-num2 })
+   //console.log(Num)
