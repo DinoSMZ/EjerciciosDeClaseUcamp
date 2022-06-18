13dc2eb5b7ebd5e00f9520f5aee59f481594f723
DinoSMZ <dinoaldair@hotmail.com>
2022-06-18 14:19:19 -0500

    a

---
+++ Retos 11 jun/main.js
@@ -0,0 +1,25 @@
+/*let Datos = [1,2,3,4,5]
+let toRemove = 1
+
+eliminarIndice(arreglo,index)
+
+let dato_filtrado = Datos.filter(function (item)){
+    return item !== toRemove=    
+}
+
+var toRemove = 1;
+var arr = [1, 2, 3, 4, 5];
+arr = arr.filter(function(item) {
+    return item !== toRemove
+});
+console.log(arr)*/
+
+let Arr = [1,2,3,4,5]
+let indice = 0
+
+borrar(); 
+function borrar(index){
+    if (index = index){
+        Arr = Arr.filter(index)
+    }}
+    console.log(Arr)
